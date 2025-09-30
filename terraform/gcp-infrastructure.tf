8624aecad9119c7b84e6ab25f6b7213e7f8c33a9
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-09-29 20:50:18 -0400

    adding dataset admin
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/gcp-infrastructure.tf
+++ terraform/gcp-infrastructure.tf
@@ -200,10 +200,9 @@ resource "google_project_iam_member" "sa_bq_job_user" {
   depends_on = [google_project_service.enable]
 }
 
-resource "google_bigquery_dataset_iam_member" "sa_dataset_editor" {
+resource "google_bigquery_iam_member" "sa_dataset_admin" {
   project    = google_project.this.project_id
-  dataset_id = google_bigquery_dataset.target.dataset_id
-  role       = "roles/bigquery.dataEditor"
+  role       = "roles/bigquery.admin"
   member     = "serviceAccount:${google_service_account.pipeline.email}"
   depends_on = [google_project_service.enable]
 }
